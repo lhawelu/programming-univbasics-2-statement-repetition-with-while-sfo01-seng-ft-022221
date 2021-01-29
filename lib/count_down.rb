ebdfdaf17c457d9e526b6285af0b9adf0961034f
Laurence Hawelu <lhawelu@gmail.com>
2021-01-29 19:40:54 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,17 +1,8 @@
-#count = 10
+count = 10
 
-#while count > 0 do
-#  puts  count
-#  count -= 1 
-#end
-
-magic_exit_number = 7 
-count = 0 
-
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!"
-  count += 1
+while count > 0 do
+  puts  count
+  count -= 1 
 end
 
-puts "love you!"
+puts "Happy New Year!"
