ff373a598dcc21eaf2c10e4abab6feb1f33fe624
Laurence Hawelu <lhawelu@gmail.com>
2021-01-29 19:40:04 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -7,8 +7,11 @@
 
 magic_exit_number = 7 
 count = 0 
+
 while count < 10 do
   break if count == magic_exit_number
   puts "I am the #{count}, I love to count!"
   count += 1
 end
+
+puts "love you!"
